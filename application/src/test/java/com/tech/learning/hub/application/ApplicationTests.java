dfb170d9b3e34e8036f878da6be4ce48fc2cc5db
cloud-borne <cloud.native.wiki@gmail.com>
2025-08-28 13:27:46 -0700

    Updated the application working directory

---
+++ application/src/test/java/com/tech/learning/hub/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.learning.hub.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
