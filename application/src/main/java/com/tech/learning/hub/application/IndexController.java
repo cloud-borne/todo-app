f7d04d1cc1303116795a3e9ba716133ee6c6a77a
cloud-borne <cloud.native.wiki@gmail.com>
2025-09-08 22:05:34 -0700

    Updated the Certificate

--- application/src/main/java/com/tech/learning/hub/application/IndexController.java
+++ application/src/main/java/com/tech/learning/hub/application/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.todo_application;
+package com.tech.learning.hub.application;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
